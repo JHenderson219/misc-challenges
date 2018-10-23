3392830aaa4d744262ea2b360c7b5de69bce5ca3
Jordan Henderson <hendersonj219@gmail.com>
2018-10-22 22:29:39 -0700

    Setup throttleAndMemo.js

---
+++ throttleAndMemo.js
@@ -0,0 +1,22 @@
+/*
+  Throttle and Memo
+  Implement Throttle and Memo as classes
+
+  Throttle
+
+*/
+
+class Suggestions {
+  /*
+    Does not matter what this does
+  */
+}
+class ViewController {
+  /*
+    Does not matter what this does
+  */
+}
+
+// ViewController will recieve input from user via typing
+// ViewController will throttle the input, then send autocomplete suggestions to the user
+// ViewController will ask Suggestions to make requests to the hypothetical back-end to get new autocomplete suggestions
