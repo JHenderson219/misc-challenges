fbbeab39229f699445e6d16b03ddbc0103d76070
Jordan Henderson <hendersonj219@gmail.com>
2018-11-11 16:40:13 -0800

    Inital setup for binary search tree

---
+++ binarySearch.js
@@ -0,0 +1,59 @@
+const DATA = [
+  {
+    "name": "Stout",
+    "age": 32,
+    "userID": "5be8cb6517f0541940fd76ff"
+  },
+  {
+    "name": "Cook",
+    "age": 29,
+    "userID": "5be8cb652afcb1df4c106f15"
+  },
+  {
+    "name": "Dolores",
+    "age": 58,
+    "userID": "5be8cb65e1b31e4c77aa0256"
+  },
+  {
+    "name": "Mia",
+    "age": 65,
+    "userID": "5be8cb65d50a5d9454da2ee4"
+  },
+  {
+    "name": "Malone",
+    "age": 23,
+    "userID": "5be8cb65232b12bab61a57d9"
+  },
+  {
+    "name": "Cathy",
+    "age": 24,
+    "userID": "5be8cb654524ca4c8da37290"
+  },
+  {
+    "name": "Molly",
+    "age": 64,
+    "userID": "5be8cb651bd75ce61d62f502"
+  },
+  {
+    "name": "Lillian",
+    "age": 24,
+    "userID": "5be8cb65827195cc9c71e2f1"
+  },
+  {
+    "name": "Lori",
+    "age": 30,
+    "userID": "5be8cb65bef0e3d9ad1f9c53"
+  },
+  {
+    "name": "Guthrie",
+    "age": 32,
+    "userID": "5be8cb659d265f3bc333e97c"
+  }
+]
+
+function makeBinTree(data) {
+  console.warn(data);
+  return;
+}
+
+makeBinTree(DATA);
